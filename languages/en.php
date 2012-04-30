9427ecfee6e7d0b8af123605b99f3fce6a88c651
Evan Winslow <evan@elgg.org>
2012-04-29 20:24:14 -0700

    Limiting comment notifications

--- languages/en.php
+++ languages/en.php
@@ -6,4 +6,13 @@ add_translation('en', array(
 	'page:blog:index:title' => 'Blog',
 
 	'page:members:index:title' => 'Missions Teams',
+	
+	'generic_comment:email:body' => "You have a new comment from %2$s.
+
+To reply or view the original item, click here:
+
+%4$s
+
+You cannot reply to this email.",
+
 ));
