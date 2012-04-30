c1fc262b2400d2b32d64e9048c6c8553b60983e9
Evan Winslow <evan.b.winslow@gmail.com>
2012-04-30 00:59:13 -0700

    Blog urls no longer contain title

--- events/init/system.php
+++ events/init/system.php
@@ -1,4 +1,7 @@
 <?php
 
+elgg_register_entity_url_handler('object', 'blog', 'missions_blog_url_handler');
+
 elgg_register_action('comments/add', dirname(dirname(__DIR__)) . '/actions/comments/add.php');
 elgg_unregister_page_handler('activity');
+
--- start.php
+++ start.php
@@ -1,3 +1,7 @@
 <?php
 
+function missions_blog_url_handler($blog) {
+  return "/blog/view/$blog->guid";
+}
+
 elgg_unregister_event_handler('create', 'object', 'object_notifications');
