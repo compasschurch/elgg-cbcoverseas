384e2f0e23e90782689c89a95d06d718bd60939d
Evan Winslow <evan@elgg.org>
2012-05-06 16:23:32 -0700

    entity convenience views available in evan framework, so not needed here anymore

--- views/default/entity/byline.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$entity = $vars['entity'];
-
-?>
-
-<?php echo elgg_echo('byline', array(evan_view_entity('link', $entity->getOwnerEntity()))); ?>
-
-<?php echo elgg_view_friendly_time($entity->time_created); ?>
--- views/default/entity/icon.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$entity = $vars['entity'];
-$size = $vars['size'];
-unset($vars['entity']);
-unset($vars['size']);
-
-echo elgg_view_entity_icon($entity, $size, $vars);
