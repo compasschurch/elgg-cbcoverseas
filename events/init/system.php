705826a8f082dde9871962713117267e7fccbf86
Evan Winslow <evan@elgg.org>
2012-06-30 15:48:46 -0700

    Removes notifications from likes

--- events/init/system.php
+++ events/init/system.php
@@ -5,6 +5,7 @@ $actions_path = dirname(dirname(__DIR__)) . '/actions';
 // Override the following actions in order to prevent the notification emails from being sent
 elgg_register_action('comments/add', "$actions_path/comments/add.php");
 elgg_register_action("messages/send", "$actions_path/messages/send.php");
+elgg_register_action("likes/add", "$actions_path/likes/add.php");
 
 // Don't want to include the title of the blog in the URL in case we send urls out via email, so override the default.
 elgg_register_entity_url_handler('object', 'blog', 'missions_blog_url_handler');
