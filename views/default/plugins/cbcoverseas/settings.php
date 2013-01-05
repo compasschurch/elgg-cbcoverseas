69d91062ae9da24bfd6547b7fdcf2e3f7ff64d6c
Evan Winslow <evan.b.winslow@gmail.com>
2013-01-05 01:33:29 -0500

    Allow inspection of next 50 users expected to receive a digest.

--- views/default/plugins/cbcoverseas/settings.php
+++ views/default/plugins/cbcoverseas/settings.php
@@ -2,6 +2,11 @@
 
 $plugin = $vars['plugin'];
 
+global $EVAN;
+
+$emailFactory = new Cbcoverseas_Digest_EmailFactory(
+    $EVAN->clock, elgg_get_site_entity(), $EVAN->views, $EVAN->db, $EVAN->i18n);
+
 ?>
 
 <div class="elgg-input-wrapper">
@@ -9,7 +14,20 @@ $plugin = $vars['plugin'];
 	<textarea name="params[posters]" class="elgg-input-textarea"><?php echo $plugin->posters; ?></textarea>
 </div>
 
-Preview of what the daily email notification looks like:
-<pre>
-	<?php echo cbcoverseas_get_activity_email_content(); ?>
-</pre>
+Next 50 users slated to receive a digest:
+<table class="elgg-table">
+    <thead>
+        <tr>
+            <td>User</td>
+            <td>Last Digest Time</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($emailFactory->getUsers(50) as $user): ?>
+        <tr>
+            <td><?php echo elgg_view('output/text', array('value' => $user->name)); ?></td>
+            <td><?php echo elgg_view('output/date', array('value' => $user->cbc_last_digest_time)); ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
