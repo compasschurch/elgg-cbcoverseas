207be614873056862500e931becc41ad104cd839
Evan Winslow <evan.b.winslow@gmail.com>
2014-10-27 12:28:18 +0000

    fix(digest): Only display 'New X' if there is actually something new in that category

--- views/email/cbcoverseas/digest.en.php
+++ views/email/cbcoverseas/digest.en.php
@@ -12,9 +12,15 @@ Hi, <?php echo $user->name; ?>!
 
 There was activity recently on CBC Overseas:
 
+<?php if ($blogs > 0): ?>
 New blogs: <?php echo $blogs; ?> 
+<?php endif; ?>
+<?php if ($photos > 0): ?>
 New photos: <?php echo $photos; ?> 
+<?php endif; ?>
+<?php if ($messages > 0): ?>
 New messages: <?php echo $messages; ?> 
+<?php endif; ?>
 
 Login at <?php echo $site->url; ?> to view them.
 
